c26473a0600ad949745077f7188655c2db94c56d
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-27 11:45:36 -0500

    setup mongodb

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const router = require('./router')
 const app = express()
+
+mongoose.connect('mongodb+srv://ethanblumenthal:oi9zArr8MUV72amF@auth-kuqgg.mongodb.net/test?retryWrites=true')
 
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
+router(app)
 
 const port = process.env.PORT || 3000
 app.listen(port)
