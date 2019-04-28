da31953c6cf364ea6d461f84be680699cdd396c9
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-28 15:26:08 -0500

    fixed cors errors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const router = require('./router')
 const { mongoURI } = require('./config')
 const app = express()
@@ -9,8 +10,9 @@ const app = express()
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 
 app.use(morgan('combined'))
+app.use(cors())
 app.use(express.json())
 router(app)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3090
 app.listen(port)
